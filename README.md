052f1009c181c88055ff38cd3f2e8d7a5fe457ae
vaibhav paliwal <15090114+vaibhavpali@users.noreply.github.com>
2021-04-26 23:34:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# poc-sms-retrieval" 
---
+++ app/src/main/java/com/vaibhav/sample/android/poc_sms_retrieval/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vaibhav.sample.android.poc_sms_retrieval;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
